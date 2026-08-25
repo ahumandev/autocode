c8b9c0c0dc57e0b4c5929bfb5147c0b594b99bb8
ahumandev <me@family.net.za>
2026-08-25 16:01:42 +0200

    chore - save current workspace

--- src/commands/repeat-as-md.ts
+++ src/commands/repeat-as-md.ts
@@ -5,5 +5,7 @@ For example:
 
 \`\`\`\`\`\`\`\`\`markdown
 Last response goes here...
-\`\`\`\`\`\`\`\`\`        
+\`\`\`\`\`\`\`\`\`
+
+Important: Convert all Mermaid diagrams to text art in the Wiki markup output. Do not include any Mermaid diagrams in the output.
 `
